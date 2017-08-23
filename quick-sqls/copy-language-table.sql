1fe13f94e05c5c4be98505df7fc314e7d98902dc
Md. Alim Ul Karim <devorg.bd@gmail.com>
2017-08-24 00:53:17 +0600

    #8, #9, #14, #19, #20, #21, #22, #23, #24 : database-change
    #8, #9, #14, #19, #20, #21, #22, #23, #24 : database-change
    
    #8 : Subscribe + Notify User : On new apps send email and notify on web
    based on subscription.
    #9 : Subscribe : Any user can subscribe app (publicly or privately or
    anonymously)
    #14 :  Database Structure Update : Blog, User Profile, Message and so on
    #19 : Database Change : Blog Parent Child Relation Added
    #20 : Database Change : Advertise Comment
    #21 : Database Change : Review Parent Child
    #22 : Review parent child
    #23 : Related App cache table
    #24 : App pricing category table

---
+++ quick-sqls/copy-language-table.sql
@@ -0,0 +1,4 @@
+INSERT INTO [D:\WORKING (SSD)\GITHUB\REVIEW-APPS.NET\REVIEWAPPS\APP_DATA\REVIEWAPPS.MDF].dbo.Language
+        ( LanguageEnglish, Code, Native )
+
+SELECT [Language],code,NativeName FROM [ReviewApps-Accounts].dbo.CountryLanguage
