93492a5af13d22607a3865d4ff5f5afd8f1dc956
Md. Alim Ul Karim <devorg.bd@gmail.com>
2017-09-03 01:01:07 +0600

    Nodejs Automation Script writing

---
+++ npm-automations/install-all.bat
@@ -0,0 +1,14 @@
+// npm install npm-install-all -g
+//npm-install-all server.js
+//npm install -g gulpfile-install
+// gulpfile-install gulpfile.js
+npm install gulp -g
+npm install gulp -D
+npm install system-sleep
+npm install gulp-less
+npm install browser-sync
+npm install gulp-header
+npm install gulp-clean-css
+npm install gulp-rename
+npm install gulp-uglify
+npm install gulp-concat
